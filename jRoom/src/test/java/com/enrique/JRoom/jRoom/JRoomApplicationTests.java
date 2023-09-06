96cd5a27d880aeb6c19e496229768641914c2cfe
Enrique Isaacs <enisaac022@student.wethinkcode.co.za>
2023-09-06 12:38:42 +0200

    Initialized spring boot project with neccesary dependancies

---
+++ jRoom/src/test/java/com/enrique/JRoom/jRoom/JRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enrique.JRoom.jRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
