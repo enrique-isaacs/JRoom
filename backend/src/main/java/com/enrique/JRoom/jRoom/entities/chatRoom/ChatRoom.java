89572f982019a12e51038d3ce989bf2d21c03525
Enrique Isaacs <enisaacs21@gmail.com>
2023-11-13 06:41:30 +0200

    Modified existing models : ChatRoom, MessageModel to better represent their table counterparts in database. ChatRoom now recognized as entity, with each field having column names. MessageModel still needs updating for new fields.

--- backend/src/main/java/com/enrique/JRoom/jRoom/entities/chatRoom/ChatRoom.java
+++ backend/src/main/java/com/enrique/JRoom/jRoom/entities/chatRoom/ChatRoom.java
@@ -1,45 +1,89 @@
 package com.enrique.JRoom.jRoom.entities.chatRoom;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.enrique.JRoom.jRoom.entities.appuser.AppUser;
+import com.enrique.JRoom.jRoom.entities.messages.MessageModel;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+
 
 
 @Getter
 @Setter
 @EqualsAndHashCode
-
+@NoArgsConstructor
 @Entity
+@Table(name = "chat_room")
 public class ChatRoom {
 
+    private static final int CODE_LENGTH = 5;
 
-    @SequenceGenerator(
-        name = "room_id_seq",
-        sequenceName = "room_id_seq",
-        allocationSize = 1
-    )
     @Id
     @GeneratedValue(
-        strategy = GenerationType.SEQUENCE,
-        generator = "room_id_seq"
+        strategy = GenerationType.IDENTITY
     )
     private Long id;
+
+
+    @Column(name = "room_name")
     private String name;
+
+    @Column(name = "room_description")
     private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "room_type")
     private ChatRoomType type;
+
+    @ManyToOne
+    @JoinColumn(name = "room_created_by")
     private AppUser createdBy;
+
+    @Column(name = "room_created_at")
     private LocalDateTime createdAt;
-    private RoomCode code; 
-    private Long numberOfUsersInRoom;
+
+    @Column(name = "room_code")
+    private String roomCode;
+
+    @Column(name = "room_messages")
+    private List<MessageModel> roomMessages;
+
+    @Column(name = "room_users")
+    private List<AppUser> roomUsers;
+
+    
+
+    public ChatRoom(String name, 
+                    String description, 
+                    ChatRoomType type, 
+                    AppUser createdBy, 
+                    LocalDateTime createdAt,
+                    List<MessageModel> roomMessages,
+                    List<AppUser> roomUsers) {
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.createdBy = createdBy;
+        this.createdAt = createdAt;
+        this.roomCode = RoomCode.generateUniqueCode(CODE_LENGTH);
+        this.roomMessages = roomMessages;
+        this.roomUsers = roomUsers;
+    }
 
     
 }
--- backend/src/main/java/com/enrique/JRoom/jRoom/entities/messages/MessageModel.java
+++ backend/src/main/java/com/enrique/JRoom/jRoom/entities/messages/MessageModel.java
@@ -1,5 +1,15 @@
 package com.enrique.JRoom.jRoom.entities.messages;
 
 public class MessageModel {
+
+
+    private Long id;
+    private String messageContent;
+    private Long senderId;
+    private Long chatRoomId;
+    private LocalDateTime createdAt;
+    private LocalDateTime readAt;
+    private boolean isRead;
+
     
 }
